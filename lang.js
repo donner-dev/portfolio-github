133cfe735c12e2d4976e3973190d8fa29d3e0e60
Donner Dev <leonor.donner@gmail.com>
2024-08-16 16:32:36 -0300

    Translation Complete
    ENG and ESP strings done. Now to make it responsive (enough)

--- lang.js
+++ lang.js
@@ -1,12 +1,29 @@
-function i18n(key) {
-    language = language ?? "en";  
-     /*that ?? this  -lil buddy is, return this if that is not found */
-    return translation[language][key];
-  }
+const flagsElement = document.getElementById("flags");
 
 
-  function i18n(key, interpolations) {
-    language = language ?? "en";
-    const value = objNavigate(translations[language], key);
-    return strObjInterpolation(value, interpolations);
+const textsToChange= document.querySelectorAll('[data-section]');
+
+
+
+const changeLanguage = async (language)=>{
+  const requestJson= await fetch(`${language}.json` )
+  const texts= await requestJson.json()
+  
+  
+  /* localizar y  modificar textos: */
+  for (const textToChange of textsToChange){
+    const section = textToChange.dataset.section
+    const value= textToChange.dataset.value;
+
+    console.log(section, value);
+    textToChange.innerHTML = texts[section][value];
   }
+};
+
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
+
+
+
